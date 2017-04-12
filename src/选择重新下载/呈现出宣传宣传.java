e1c6df6587a838701047a2189308a201fe5bbcd7
段红坤 <951747463@qq.com>
2017-04-12 10:36:48 +0800

    我

--- src/选择重新下载/呈现出宣传宣传.java
+++ src/选择重新下载/呈现出宣传宣传.java
@@ -4,10 +4,14 @@
  */
 package 选择重新下载;
 
+import org.eclipse.jdt.internal.compiler.ast.SynchronizedStatement;
+
 /**
  * @author 段红坤
  *2017年4月12日
  */
 public class 呈现出宣传宣传 {
-
+public static void main(String[] args) {
+	System.out.println("aaaa");
+}
 }
